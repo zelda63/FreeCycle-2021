6bed4fea2c8f5d1a3c0e2ebb892f0eb45cc68ef4
Harshalkumar Patel <patel81781@saskpolytech.ca>
2021-05-26 18:10:03 -0600

    new

--- FreeCycle2/Controllers/AuthController.cs
+++ FreeCycle2/Controllers/AuthController.cs
@@ -16,8 +16,25 @@ namespace FreeCycle2.Controllers
             return View();
         }
 
+
+
+        public ActionResult RemoveData()
+        {
+
+            var fn = new HttpCookie("first_name");
+            fn.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(fn);
+
+            var ui = new HttpCookie("user_id");
+            ui.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(ui);
+            return RedirectToAction("../Auth/Login");
+        }
+
+
+
         [HttpPost]
-        public ActionResult Login(string email, string password)
+        public ActionResult Login(string email, string login_pwd_encry)
         {
             if (email == null)
                 return View();
@@ -31,7 +48,7 @@ namespace FreeCycle2.Controllers
 
             if (customer.email.Equals(email))
             {
-                if (customer.password != password)
+                if (customer.login_pwd_encry != login_pwd_encry)
                 {
                     ViewData["warningPassword"] = "Wrong password, Please try again";
                     return View();
@@ -42,6 +59,14 @@ namespace FreeCycle2.Controllers
                 ViewData["warning"] = "Invalid username";
                 return View();
             }
+
+            HttpCookie user_id = new HttpCookie("user_id");
+            HttpCookie first_name = new HttpCookie("first_name");
+
+            user_id.Value = customer.user_id.ToString();
+            first_name.Value = customer.first_name;
+            HttpContext.Response.Cookies.Add(user_id);
+            HttpContext.Response.Cookies.Add(first_name);
 
             return RedirectToAction("../Home/Item");
         }
