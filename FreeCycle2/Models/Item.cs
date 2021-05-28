4b2fadba5e87087bf376d3436ecdb1a0b44eca0d
Harshalkumar Patel <patel81781@saskpolytech.ca>
2021-05-28 16:21:18 +0000

    Item.cs edited online with Bitbucket

--- FreeCycle2/Models/Item.cs
+++ FreeCycle2/Models/Item.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
 
 namespace FreeCycle2.Models
 {
     public class Item
     {
-        public int item_id { get; set; }
+        public int EditIndex2 { get; set; }
+        public List<Item> allItems { get; set; }
 
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+   
+        public int item_id { get; set; }
+        public bool IsEditable2 { get; set; }
 
         public DateTime create_date { get; set; }
         public DateTime last_renewed_on { get; set; }
@@ -21,17 +29,11 @@ namespace FreeCycle2.Models
         [Display(Name = "User")]
         public int user_id { get; set; }
 
-        //[ForeignKey("user_id")]
-        //public virtual User User { get; set; }
-
         [Display(Name = "Category")]
         public int category_id { get; set; }
 
-        //[ForeignKey("category_id")]
-       // public virtual Category Category { get; set; }
-
         public char is_active { get; set; }
-        
+
         public byte[] image { get; set; }
 
     }
